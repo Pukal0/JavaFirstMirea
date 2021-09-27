baa2844f3cc4ed7f153a096d1dba0d211382a6ff
Artem <artempervov2003@ya.ru>
2021-09-27 20:38:50 +0300

    test commit

--- src/ru/mirea/test/TestFromTeacher.java
+++ src/ru/mirea/test/TestFromTeacher.java
@@ -4,7 +4,7 @@ public class TestFromTeacher {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Это класс для тестирования!;");
+        System.out.println("Это класс для тестирования;");
         System.out.println("Если он запускается - скорее всего, у вас всё настроено правильно;");
         System.out.println("Вы можете удалить этот класс и пакет.");
     }
